00f6eb7b47fe58b0428f2d46205dc83d59bb8e25
Renaud Dumont <renaud.dumont@live.fr>
2019-08-02 23:02:41 +0200

    fixed cow reducer

--- sosfaim/lib/reducers/cow_module_reducer.dart
+++ sosfaim/lib/reducers/cow_module_reducer.dart
@@ -1,12 +1,9 @@
 // We create the State reducer by combining many smaller reducers into one!
+import 'package:redux/redux.dart';
 import 'package:sosfaim/models/app_state.dart';
 
 AppState cowModuleReducer(AppState state, action) {
-  return AppState(
-      isLoading: state.isLoading,
-      modules: state.modules,
-      dayCount: state.dayCount,
-      capital: state.capital,
-      milkPrice: state.milkPrice,
-      milkProduction: state.milkProduction);
+  return combineReducers<AppState>([
+   
+  ])(state, action);
 }
