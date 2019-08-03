d53a299e3c2af1645d9ca9b23bca271ee015cc46
Bruno Moureau <bruno.moureau@hotmail.com>
2019-08-03 13:38:30 +0200

    added cow icon + padding

--- sosfaim/lib/components/CowManager/cow_manager_view.dart
+++ sosfaim/lib/components/CowManager/cow_manager_view.dart
@@ -23,47 +23,82 @@ class CowManagerView extends StatelessWidget {
     final selectedCowNumberInt = selectedCowNumber.toInt();
     final cowButtonText = selectedCowNumber < 0 ? 'Vendre $selectedCowNumberInt vache(s)': 'Acheter $selectedCowNumberInt vache(s)';
     
-    return new Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget> [
-        new Container(
-          // A fixed-height child.
-          height: 40.0,
-          padding: const EdgeInsets.all(10.0),
-          child: new LinearProgressIndicator(
-            value: totalCowNumber / 100.0,
-            backgroundColor: Colors.grey,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
-          )
-        ),
-        new Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget> [
-            new Text("Nombre de vaches :"),
-            new Slider(
-              min: -10,
-              divisions: 20,
-              max: 10,
-              value: selectedCowNumber,
-              onChanged: onCowNumberValuedChanged
+    return new Container(
+      child: new Column(
+        children: <Widget> [
+          new Container(
+            height: 40.0,
+            padding: const EdgeInsets.only(top: 10.0, right: 10.0, bottom: 10.0),
+            child: new Row(
+              children: <Widget> [
+                new Image.asset(
+                  'assets/images/cow_icon.png',
+                  width: 48.0,
+                  height: 48.0,
+                  fit: BoxFit.contain,
+                ),
+                new Expanded(
+                  child: new Stack(
+                    children: <Widget> [
+                      new Container(
+                        height: 40.0,
+                        child: new LinearProgressIndicator(
+                          value: totalCowNumber / 100.0,
+                          backgroundColor: Colors.grey,
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.teal)
+                        )
+                      ),
+                      new Container(
+                        height: 40.0,
+                        child: new Align(
+                          alignment: Alignment.center,
+                          child : new Text(
+                            '$totalCowNumber/100',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                            ),
+                          )
+                        )
+                      )
+                    ]
+                  )
+                )
+              ]
             )
-          ]
-        ),
-        new Visibility(
-          visible: canShowCowButton,
-          child: new RaisedButton(
-            elevation: 2,
-            child: Text(cowButtonText),
-            onPressed: () {
-              //Insert event to be fired up when button is clicked here
-              //in this case, this increments our countValue variable by one.
-              //setState(() => _progressValue += 0.1);
-            }
+          ),
+          new Container(
+            height: 40.0,
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            child: new Row(
+              children: <Widget> [
+                new Text("Nombre de vaches :"),
+                 new Expanded(
+                  child: new Slider(
+                    min: -10,
+                    divisions: 20,
+                    max: 10,
+                    value: selectedCowNumber,
+                    onChanged: onCowNumberValuedChanged
+                  )
+                )
+              ]
+            )
+          ),
+          new Visibility(
+            visible: canShowCowButton,
+            child: new RaisedButton(
+              elevation: 2,
+              child: Text(cowButtonText),
+              onPressed: () {
+                //Insert event to be fired up when button is clicked here
+                //in this case, this increments our countValue variable by one.
+                //setState(() => _progressValue += 0.1);
+              }
+            )
           )
-        )
-      ]
+        ]
+      )
     );
   }
 }
